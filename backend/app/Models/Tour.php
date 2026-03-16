eaae4a8d55636e173893aff756b3af17b44d46b9
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-16 11:27:02 +0000

    updaate User Model with relatioons

--- backend/app/Models/Tour.php
+++ backend/app/Models/Tour.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use App\Enums\DifficultyLevel;
 use Illuminate\Database\Eloquent\Model;
 
-class Tour extends Model
-{
-    //
+class Tour extends Model {
+    protected $fillable = ['title', 'description', 'location', 'price', 'difficulty', 'max_spots', 'current_bookings', 'guide_id'];
+
+    protected $casts = [
+        'difficulty' => DifficultyLevel::class,
+        'price' => 'float',
+    ];
+
+    // relation wih guide
+    public function guide() {
+        return $this->belongsTo(User::class, 'guide_id');
+    }
+
+    // relation with 
+    public function images() {
+        return $this->hasMany(Image::class);
+    }
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,47 +11,49 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use  HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-     protected $fillable = ['fullname', 'email', 'password', 'role', 'is_verified'];
+    protected $fillable = ['fullname', 'email', 'password', 'role', 'is_verified'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
+    protected $hidden = ['password', 'remember_token'];
+
+    protected $casts = [
+        'role' => UserRole::class,
+        'is_verified' => 'boolean',
+        'password' => 'hashed',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'role' => UserRole::class,
-        ];
-    }
-
+    // Guide relations
     public function tours() {
         return $this->hasMany(Tour::class, 'guide_id');
     }
+    public function verificationRequest() {
+        return $this->hasOne(Verification::class, 'guide_id');
+    }
+    public function receivedBookings() {
+        return $this->hasMany(Booking::class, 'guide_id');
+    }
 
-
+    // Traveler relations
     public function bookings() {
         return $this->hasMany(Booking::class, 'traveler_id');
     }
+    public function reviews() {
+        return $this->hasMany(Review::class, 'traveler_id');
+    }
+    public function favorites() {
+        return $this->hasMany(Favorite::class, 'traveler_id');
+    }
 
+    public function reports() {
+        return $this->hasMany(TripReport::class, 'traveler_id');
+    }
+
+    //Admin relations
+    public function approvedVerifications() {
+        return $this->hasMany(Verification::class, 'admin_id');
+    }
+    public function managedReports() {
+        return $this->hasMany(TripReport::class, 'admin_id');
+    }
 }
