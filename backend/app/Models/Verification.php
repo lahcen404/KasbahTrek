5bf764cf560d76db4eaa66a736ac945f865fe1ad
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-16 11:31:17 +0000

    create Verification Model

--- backend/app/Models/Verification.php
+++ backend/app/Models/Verification.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Verification extends Model
-{
-    //
+class Verification extends Model {
+    protected $fillable = ['file_url', 'status', 'guide_id', 'admin_id'];
+    protected $casts = ['status' => \App\Enums\Status::class];
+    public function guide() { return $this->belongsTo(User::class, 'guide_id'); }
+    public function admin() { return $this->belongsTo(User::class, 'admin_id'); }
 }
