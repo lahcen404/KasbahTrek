bf7d2f862a5d62a0977fe0bef44da684225477af
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-16 11:30:17 +0000

    create Favorite Model

--- backend/app/Models/Favorite.php
+++ backend/app/Models/Favorite.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Favorite extends Model
-{
-    //
+class Favorite extends Model {
+    protected $fillable = ['traveler_id', 'tour_id'];
+    public function traveler() { return $this->belongsTo(User::class, 'traveler_id'); }
+    public function tour() { return $this->belongsTo(Tour::class); }
 }
