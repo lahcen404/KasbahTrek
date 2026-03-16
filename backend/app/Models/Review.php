4ac2501273c98564524262a6eaa912f41c8846cb
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-16 11:29:50 +0000

    create Review Model

--- backend/app/Models/Review.php
+++ backend/app/Models/Review.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Review extends Model
-{
-    //
+class Review extends Model {
+    protected $fillable = ['rating', 'comment', 'traveler_id', 'tour_id'];
+    public function traveler() { return $this->belongsTo(User::class, 'traveler_id'); }
+    public function tour() { return $this->belongsTo(Tour::class); }
 }
