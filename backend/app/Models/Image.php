be9b3e2a0d47924e591aae8b5ded09349bc6e498
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-16 11:29:12 +0000

    create Image Model

--- backend/app/Models/Image.php
+++ backend/app/Models/Image.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Image extends Model
-{
-    //
+class Image extends Model {
+    protected $fillable = ['path', 'tour_id'];
+    public function tour() { return $this->belongsTo(Tour::class); }
 }
