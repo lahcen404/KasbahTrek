80dcbcf68946cfa1773583587c8cfe2bb23235bc
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-17 14:12:45 +0000

    create Booking Repository

---
+++ backend/app/Repositories/BookingRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\BookingRepositoryInterface;
+use App\Models\Booking;
+use App\Models\Tour;
+use Illuminate\Support\Facades\DB;
+
+class BookingRepository implements BookingRepositoryInterface
+{
+    public function create(array $data)
+    {
+        // fiinf the tour
+        $tour = Tour::findOrFail($data['tour_id']);
+
+        // cheeck if tour full
+        if ($tour->current_bookings >= $tour->max_spots) {
+            throw new \Exception("This tour is already full!!!");
+        }
+
+        // creaate the booking
+        return DB::transaction(function () use ($data, $tour) {
+            $booking = Booking::create([
+                'date' => $data['date'],
+                'total_price' => $tour->price,
+                'status' => 'PENDING',
+                'traveler_id' => auth()->id(),
+                'tour_id' => $tour->id,
+                'guide_id' => $tour->guide_id,
+            ]);
+
+            
+            $tour->increment('current_bookings');
+
+            return $booking;
+        });
+    }
+
+    public function getByTraveler(int $travelerId)
+    {
+        return Booking::where('traveler_id', $travelerId)->with('tour')->get();
+    }
+
+    public function getByGuide(int $guideId)
+    {
+        return Booking::where('guide_id', $guideId)->with(['tour', 'traveler'])->get();
+    }
+
+    public function updateStatus(int $id, string $status)
+    {
+        $booking = Booking::findOrFail($id);
+        $booking->update(['status' => $status]);
+        return $booking;
+    }
+}
