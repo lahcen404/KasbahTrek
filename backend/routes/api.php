1eaf6d7fbf67711245ae3ce7d8e6dce85d6916ee
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-17 12:19:31 +0000

    add tour api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,13 +1,27 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TourController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// PUBLIIIC routes
+
+// auth routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+// tours
+Route::get('/tours', [TourController::class, 'index']);
+Route::get('/tours/{id}', [TourController::class, 'show']);
+
+
+// PROTEEECTED routes
 Route::middleware(['auth.custom'])->group(function() {
     Route::post('/logout',[AuthController::class, 'logout']);
 
+});
+
+Route::middleware(['auth.custom', 'role:guide'])->group(function() {
+    Route::post('/tours', [TourController::class, 'store']);
 });
