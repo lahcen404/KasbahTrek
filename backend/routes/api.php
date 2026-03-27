9532b3d3e6d240e4b8a8a4dbdcc812124660743d
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-27 19:20:23 +0100

    create routes of admin manage users

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookingController;
 use App\Http\Controllers\Api\FavoriteController;
+use App\Http\Controllers\Api\Admin\UserController as AdminUserController;
 use App\Http\Controllers\Api\ReviewController;
 use App\Http\Controllers\Api\TourController;
 use Illuminate\Support\Facades\Route;
@@ -49,4 +50,11 @@ Route::middleware(['auth.custom', 'role:TRAVELER'])->group(function () {
 
     Route::post('/reviews', [ReviewController::class, 'add']);
     Route::get('/my-reviews', [ReviewController::class, 'myReviews']);
+});
+
+Route::middleware(['auth.custom', 'role:ADMIN'])->prefix('admin')->group(function () {
+    Route::get('/users', [AdminUserController::class, 'index']);
+    Route::get('/users/{id}', [AdminUserController::class, 'show']);
+    Route::put('/users/{id}', [AdminUserController::class, 'update']);
+    Route::delete('/users/{id}', [AdminUserController::class, 'destroy']);
 });
