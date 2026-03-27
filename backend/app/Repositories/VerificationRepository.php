67f1414287300d2c478350638322ced94d1b68c7
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-27 19:55:45 +0100

    create Verification Repository Interface & Verification Repository

---
+++ backend/app/Repositories/VerificationRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\VerificationRepositoryInterface;
+use App\Models\Verification;
+use App\Models\User;
+
+class VerificationRepository implements VerificationRepositoryInterface
+{
+    public function createRequest(array $data)
+    {
+        $data['status'] = 'PENDING';
+        return Verification::create($data);
+    }
+
+    public function getPendingRequests()
+    {
+        // get all peending requests
+        return Verification::where('status', 'PENDING')->get();
+    }
+
+    public function updateStatus($id, $status)
+    {
+        $verification = Verification::findOrFail($id);
+        
+        $verification->update([
+            'status' => strtoupper($status)
+        ]);
+
+        // check status and update user is_verified
+        if (strtoupper($status) === 'APPROVED') {
+            User::where('id', $verification->guide_id)->update(['is_verified' => true]);
+        } elseif (strtoupper($status) === 'REJECTED') {
+            User::where('id', $verification->guide_id)->update(['is_verified' => false]);
+        }
+
+        return $verification;
+    }
+}
