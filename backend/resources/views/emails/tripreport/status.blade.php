c29032a81e22f9781539334f909f4bf802949a88
lahcen404 <lahcen.maskour2003@gmail.com>
2026-03-31 11:19:38 +0100

    create View for email trip report

--- backend/resources/views/emails/tripreport/status.blade.php
+++ backend/resources/views/emails/tripreport/status.blade.php
@@ -1,11 +1,18 @@
 <x-mail::message>
-# Introduction
+# Trip Report Status Update
 
-The body of your message.
+Hello {{ $report->traveler->name }},
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+The status of the Trip Report you submitted has been updated by an Admin.
+Your report's current status is: **{{ $report->status->value ?? $report->status }}**.
+
+@if($report->status === 'APPROVED' || ($report->status->value ?? '') === 'APPROVED')
+Your report has been approved. The problematic tour/guide is under review. Thank you for keeping Kasbah Trek safe!
+@elseif($report->status === 'REJECTED' || ($report->status->value ?? '') === 'REJECTED')
+Unfortunately, your report was rejected. The Admin determined there was not enough evidence or the issue did not violate our policies.
+@elseif($report->status === 'RESOLVED' || ($report->status->value ?? '') === 'RESOLVED')
+Your report has been marked as resolved! The issue you raised has been successfully handled.
+@endif
 
 Thanks,<br>
 {{ config('app.name') }}
